87490ae3acc2b7403b5ce5c789486480b6f70647
Dendeey <davidsim1035@gmail.com>
2023-02-17 14:59:34 +0100

    Yep Wordpress

--- wp-content/themes/yep/functions.php
+++ wp-content/themes/yep/functions.php
@@ -2,4 +2,14 @@
 
 add_theme_support('post-thumbnails');
 
+// Nous avons déclaré un menu qui s'appellera "Menu de navigation".
+
+function register_my_menus() 
+{ 
+    register_nav_menus( array( 
+    'header-menu' => __( 'Menu de navigation' ))); 
+}
+
+add_action( 'init', 'register_my_menus' );
+
 ?>
